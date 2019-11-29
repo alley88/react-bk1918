e32763a842173459fb398e9ecd0823d46598cbc6
zhao <437389128@qq.com>
2019-11-29 17:42:56 +0800

    项目初始化

---
+++ src/components/tabBar/index.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+import {TabBarContainer} from "./styled"
+import {TabBarRoute} from "router"
+import {withRouter} from "react-router-dom";
+class TabBar extends Component {
+    render() {
+        let {path} = this.props;
+        return (
+            <TabBarContainer>
+                    <ul>
+                       {
+                           TabBarRoute.map((item)=>(
+                               <li key={item.path} onClick={this.handleTo.bind(this,item.path)} className={path===item.path?'active':""}>
+                                    <i className="iconfont">{item.icon}</i>
+                                    <span>{item.text}</span>
+                               </li>
+                           ))
+                       }
+                    </ul>
+            </TabBarContainer>
+        )
+    }
+    handleTo(path){
+        this.props.history.push(path);
+    }
+}
+
+export default TabBar;
